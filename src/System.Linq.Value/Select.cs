e0b278582938a17b0efa7c53812c003aefe64982
Juhász Péter <peter.juhasz@live.com>
2020-07-03 16:37:19 +0200

    Select

---
+++ src/System.Linq.Value/Select.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Linq.Value
+{
+    public static partial class EnumerableExtensions
+    {
+        public static SelectEnumerable<T, TResult> ValueSelect<T, TResult>(this IEnumerable<T> source, Func<T, TResult> selector) =>
+            new SelectEnumerable<T, TResult>(source, selector);
+
+        public static SelectWithIndexEnumerable<T, TResult> ValueSelect<T, TResult>(this IEnumerable<T> source, Func<T, int, TResult> selector) =>
+            new SelectWithIndexEnumerable<T, TResult>(source, selector);
+
+
+        public readonly struct SelectEnumerable<T, TResult> : IEnumerable<TResult>
+        {
+            public SelectEnumerable(IEnumerable<T> source, Func<T, TResult> selector)
+            {
+                this.source = source;
+                this.selector = selector;
+            }
+
+            private readonly IEnumerable<T> source;
+            private readonly Func<T, TResult> selector;
+
+            public Enumerator GetEnumerator() => new Enumerator(source, selector);
+
+            IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+            IEnumerator<TResult> IEnumerable<TResult>.GetEnumerator() => this.GetEnumerator();
+
+            public struct Enumerator : IEnumerator<TResult>
+            {
+                public Enumerator(IEnumerable<T> source, Func<T, TResult> selector)
+                {
+                    Current = default!;
+                    enumerator = source.GetEnumerator();
+                    this.selector = selector;
+                }
+
+                private readonly IEnumerator<T> enumerator;
+                private readonly Func<T, TResult> selector;
+
+                public TResult Current { get; private set; }
+
+                object? IEnumerator.Current => this.Current;
+
+                public bool MoveNext()
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        Current = selector(enumerator.Current);
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                public void Reset()
+                {
+                    enumerator.Reset();
+                }
+
+                public void Dispose() => enumerator.Dispose();
+            }
+        }
+
+        public readonly struct SelectWithIndexEnumerable<T, TResult> : IEnumerable<TResult>
+        {
+            public SelectWithIndexEnumerable(IEnumerable<T> source, Func<T, int, TResult> selector)
+            {
+                this.source = source;
+                this.selector = selector;
+            }
+
+            private readonly IEnumerable<T> source;
+            private readonly Func<T, int, TResult> selector;
+
+            public Enumerator GetEnumerator() => new Enumerator(source, selector);
+
+            IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+            IEnumerator<TResult> IEnumerable<TResult>.GetEnumerator() => this.GetEnumerator();
+
+            public struct Enumerator : IEnumerator<TResult>
+            {
+                public Enumerator(IEnumerable<T> source, Func<T, int, TResult> selector)
+                {
+                    Current = default!;
+                    enumerator = source.GetEnumerator();
+                    this.selector = selector;
+                    index = -1;
+                }
+
+                private readonly IEnumerator<T> enumerator;
+                private readonly Func<T, int, TResult> selector;
+
+                public TResult Current { get; private set; }
+
+                object? IEnumerator.Current => this.Current;
+
+                private int index;
+
+                public bool MoveNext()
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        index++;
+                        Current = selector(enumerator.Current, index);
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                public void Reset()
+                {
+                    enumerator.Reset();
+                    index = -1;
+                }
+
+                public void Dispose() => enumerator.Dispose();
+            }
+        }
+    }
+}
---
+++ tests/System.Linq.Value.Tests/SelectTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Linq.Value.Tests
+{
+    [TestClass]
+    public class SelectTests
+    {
+        [TestMethod]
+        public void Select()
+        {
+            var array = new int[] { 2, 1, 5 };
+            var result = array.ValueSelect(i => 2 * i).ToArray();
+            var expected = new int[] { 4, 2, 10 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SelectWithIndex()
+        {
+            var array = new int[] { 2, 1, 5 };
+            var result = array.ValueSelect((v, i) => i).ToArray();
+            var expected = new int[] { 0, 1, 2 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
