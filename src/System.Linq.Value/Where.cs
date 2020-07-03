7bac4ea4da71052b2be0fddfb3e9fedd9835d9fa
Juhász Péter <peter.juhasz@live.com>
2020-07-03 18:57:02 +0200

    where with index

--- src/System.Linq.Value/Where.cs
+++ src/System.Linq.Value/Where.cs
@@ -9,6 +9,9 @@ namespace System.Linq.Value
         public static WhereEnumerable<T> ValueWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate) =>
             new WhereEnumerable<T>(source, predicate);
 
+        public static WhereWithIndexEnumerable<T> ValueWhere<T>(this IEnumerable<T> source, Func<T, int, bool> predicate) =>
+            new WhereWithIndexEnumerable<T>(source, predicate);
+
         public readonly struct WhereEnumerable<T> : IEnumerable<T>
         {
             public WhereEnumerable(IEnumerable<T> source, Func<T, bool> predicate)
@@ -60,6 +63,68 @@ namespace System.Linq.Value
                 public void Reset()
                 {
                     enumerator.Reset();
+                }
+
+                public void Dispose() => enumerator.Dispose();
+            }
+        }
+
+        public readonly struct WhereWithIndexEnumerable<T> : IEnumerable<T>
+        {
+            public WhereWithIndexEnumerable(IEnumerable<T> source, Func<T, int, bool> predicate)
+            {
+                this.source = source;
+                this.predicate = predicate;
+            }
+
+            private readonly IEnumerable<T> source;
+            private readonly Func<T, int, bool> predicate;
+
+            public Enumerator GetEnumerator() => new Enumerator(source, predicate);
+
+            IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+            IEnumerator<T> IEnumerable<T>.GetEnumerator() => this.GetEnumerator();
+
+            public struct Enumerator : IEnumerator<T>
+            {
+                public Enumerator(IEnumerable<T> source, Func<T, int, bool> predicate)
+                {
+                    Current = default!;
+                    enumerator = source.GetEnumerator();
+                    this.predicate = predicate;
+                    index = -1;
+                }
+
+                private readonly IEnumerator<T> enumerator;
+                private readonly Func<T, int, bool> predicate;
+
+                public T Current { get; private set; }
+
+                object? IEnumerator.Current => this.Current;
+
+                private int index;
+
+                public bool MoveNext()
+                {
+                    while (enumerator.MoveNext())
+                    {
+                        index++;
+                        var current = enumerator.Current;
+                        if (predicate(current, index))
+                        {
+                            Current = current;
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+
+                public void Reset()
+                {
+                    enumerator.Reset();
+                    index = -1;
                 }
 
                 public void Dispose() => enumerator.Dispose();
