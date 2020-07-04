b8712f6e05547e0146d3e0c9d349f5179f5eac51
Juhász Péter <peter.juhasz@live.com>
2020-07-04 09:10:19 +0200

    join

---
+++ src/System.Linq.Value/Join.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Linq.Value
+{
+    public static partial class EnumerableExtensions
+    {
+        public static JoinEnumerable<TLeft, TRight, TKey, TResult> ValueJoin<TLeft, TRight, TKey, TResult>(
+            this IEnumerable<TLeft> source,
+            IEnumerable<TRight> second,
+            Func<TLeft, TKey> leftKeySelector,
+            Func<TRight, TKey> rightKeySelector,
+            Func<TLeft, TRight, TResult> resultSelector,
+            IEqualityComparer<TKey> keyComparer
+        ) =>
+            new JoinEnumerable<TLeft, TRight, TKey, TResult>(source, second, leftKeySelector, rightKeySelector, resultSelector, keyComparer);
+
+        public static JoinEnumerable<TLeft, TRight, TKey, TResult> ValueJoin<TLeft, TRight, TKey, TResult>(
+           this IEnumerable<TLeft> source,
+           IEnumerable<TRight> second,
+           Func<TLeft, TKey> leftKeySelector,
+           Func<TRight, TKey> rightKeySelector,
+           Func<TLeft, TRight, TResult> resultSelector
+       ) =>
+            source.ValueJoin(second, leftKeySelector, rightKeySelector, resultSelector, EqualityComparer<TKey>.Default);
+
+
+        public readonly struct JoinEnumerable<TLeft, TRight, TKey, TResult> : IEnumerable<TResult>
+        {
+            public JoinEnumerable(
+                IEnumerable<TLeft> source,
+                IEnumerable<TRight> second,
+                Func<TLeft, TKey> leftKeySelector,
+                Func<TRight, TKey> rightKeySelector,
+                Func<TLeft, TRight, TResult> resultSelector,
+                IEqualityComparer<TKey> keyComparer
+            )
+            {
+                this.source = source;
+                this.second = second;
+                this.leftKeySelector = leftKeySelector;
+                this.rightKeySelector = rightKeySelector;
+                this.resultSelector = resultSelector;
+                this.keyComparer = keyComparer;
+            }
+
+            private readonly IEnumerable<TLeft> source;
+            private readonly IEnumerable<TRight> second;
+            private readonly Func<TLeft, TKey> leftKeySelector;
+            private readonly Func<TRight, TKey> rightKeySelector;
+            private readonly Func<TLeft, TRight, TResult> resultSelector;
+            private readonly IEqualityComparer<TKey> keyComparer;
+
+            public Enumerator GetEnumerator() => new Enumerator(source, second, leftKeySelector, rightKeySelector, resultSelector, keyComparer);
+
+            IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+            IEnumerator<TResult> IEnumerable<TResult>.GetEnumerator() => this.GetEnumerator();
+
+            public struct Enumerator : IEnumerator<TResult>
+            {
+                public Enumerator(
+                    IEnumerable<TLeft> source,
+                    IEnumerable<TRight> second,
+                    Func<TLeft, TKey> leftKeySelector,
+                    Func<TRight, TKey> rightKeySelector,
+                    Func<TLeft, TRight, TResult> resultSelector,
+                    IEqualityComparer<TKey> keyComparer
+                )
+                {
+                    Current = default!;
+                    this.enumerator = source.GetEnumerator();
+                    this.second = second;
+                    this.leftKeySelector = leftKeySelector;
+                    this.rightKeySelector = rightKeySelector;
+                    this.resultSelector = resultSelector;
+                    this.keyComparer = keyComparer;
+                }
+
+                private readonly IEnumerator<TLeft> enumerator;
+                private readonly IEnumerable<TRight> second;
+                private readonly Func<TLeft, TKey> leftKeySelector;
+                private readonly Func<TRight, TKey> rightKeySelector;
+                private readonly Func<TLeft, TRight, TResult> resultSelector;
+                private readonly IEqualityComparer<TKey> keyComparer;
+
+                public TResult Current { get; private set; }
+
+                object? IEnumerator.Current => this.Current;
+
+
+                public bool MoveNext()
+                {
+                    var keyComparer = this.keyComparer;
+                    var rightKeySelector = this.rightKeySelector;
+
+                    if (enumerator.MoveNext())
+                    {
+                        var current = enumerator.Current;
+                        var key = leftKeySelector(current);
+                        var other = second.SingleOrDefault(s => keyComparer.Equals(key, rightKeySelector(s)));
+                        Current = resultSelector(current, other);
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                public void Reset()
+                {
+                    enumerator.Reset();
+                }
+
+                public void Dispose()
+                {
+                    enumerator.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ tests/System.Linq.Value.Tests/JoinTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Linq.Value.Tests
+{
+    [TestClass]
+    public class JoinTests
+    {
+        [TestMethod]
+        public void Join()
+        {
+            var array = new int[] { 2, 1, 5 };
+            var result = array.ValueJoin(new[] { 2, 5 }, x => x, y => y, (x, y) => x * y).ToArray();
+            var expected = new int[] { 4, 0, 25 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
