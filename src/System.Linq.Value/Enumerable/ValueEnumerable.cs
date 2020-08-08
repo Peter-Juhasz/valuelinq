5dedafeb4b3650e5c57c6a61b8dec4b3a435107c
Juhász Péter <peter.juhasz@live.com>
2020-08-08 22:05:17 +0200

    value enumerable: empty, count 1

---
+++ src/System.Linq.Value/Enumerable/ValueEnumerable.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class ValueEnumerable
+    {
+        public static Value1Enumerable<T> Create<T>(T value) => new Value1Enumerable<T>(value);
+
+        public static EmptyEnumerable<T> Empty<T>() => new EmptyEnumerable<T>();
+
+        public readonly struct Value1Enumerable<T> : IEnumerable<T>
+        {
+            public Value1Enumerable(T value) => Value = value;
+
+            public T Value { get; }
+
+            public IEnumerator<T> GetEnumerator() => new Value1Enumerator(Value);
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            public struct Value1Enumerator : IEnumerator<T>
+            {
+                public Value1Enumerator(T value)
+                {
+                    _value = value;
+                    _started = false;
+                    Current = default!;
+                }
+
+                private T _value;
+
+                private bool _started;
+
+                public T Current { get; private set; }
+
+                object IEnumerator.Current => Current!;
+
+                public bool MoveNext()
+                {
+                    if (!_started)
+                    {
+                        _started = true;
+                        Current = _value;
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                public void Reset()
+                {
+                    _started = false;
+                    Current = default!;
+                }
+
+                public void Dispose() { }
+            }
+        }
+
+        public readonly struct EmptyEnumerable<T> : IEnumerable<T>
+        {
+            public IEnumerator<T> GetEnumerator() => new EmptyEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            public struct EmptyEnumerator : IEnumerator<T>
+            {
+                public T Current => throw new InvalidOperationException();
+
+                object IEnumerator.Current => Current!;
+
+                public bool MoveNext() => false;
+
+                public void Reset() { }
+
+                public void Dispose() { }
+            }
+        }
+    }
+}
---
+++ tests/System.Linq.Value.Tests/Enumerable/ValueEnumerableTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Linq.Value.Tests
+{
+    [TestClass]
+    public class ValueEnumerableTests
+    {
+        [TestMethod]
+        public void Empty()
+        {
+            var enumerable = ValueEnumerable.Empty<int>();
+            Assert.AreEqual(0, enumerable.Count());
+        }
+
+        [TestMethod]
+        public void Value1()
+        {
+            var enumerable = ValueEnumerable.Create(5);
+            Assert.AreEqual(1, enumerable.Count());
+            Assert.AreEqual(5, enumerable.Single());
+        }
+    }
+}
